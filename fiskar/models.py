a0127fddb3b2b76efeba6ff7897a87a51fd05ca2
Ekman <my.ekman@sweco.se>
2024-01-10 10:28:58 +0100

    fish and like model

--- fiskar/models.py
+++ fiskar/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Fish(models.Model):
+    FISH_TYPES = [
+        ('goldfish', 'Goldfish'),
+        ('clownfish', 'Clownfish'),
+        ('anglerfish', 'Anglerfish'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    like_count = models.PositiveIntegerField(default=0)
+    fish_type = models.CharField(max_length=20, choices=FISH_TYPES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.get_fish_type_display()} #{self.id}: {self.message} {self.user}"
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fish = models.ForeignKey(Fish, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Detta kommer att se till att en användare bara kan "like" en fisk en gång
+        unique_together = ('user', 'fish')
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.fish.id}"
