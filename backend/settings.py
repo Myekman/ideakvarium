85f3b2cbc414d12a02854632d2d27a485c6d2e57
Ekman <my.ekman@sweco.se>
2024-01-15 21:41:34 +0100

    login error frontend debug

--- backend/settings.py
+++ backend/settings.py
@@ -64,6 +64,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CSRF_TRUSTED_ORIGINS = (
+    'http://127.0.0.1:8000/', 'http://localhost:3000/'
+)
+
 #----------------------------------------------------------------Rekomenderas för produktion
 # CORS_ALLOW_ALL_ORIGINS = False
 # CORS_ALLOWED_ORIGINS = [
