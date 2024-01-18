ed854cde77dd0f34735601f861a4db0050ca7357
Ekman <my.ekman@sweco.se>
2024-01-18 13:06:33 +0100

    uninstall babel

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,11 @@
-{
-  "plugins": ["@babel/plugin-proposal-private-property-in-object"]
-}
+module.exports = {
+    presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        // andra presets du använder...
+      ],
+    plugins: [
+      // Dina övriga Babel-plugins (om några)
+      "@babel/plugin-transform-private-property-in-object",
+    ],
+  };
