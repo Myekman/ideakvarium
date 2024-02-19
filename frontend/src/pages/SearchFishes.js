f2b23aa1a9fc324b4a5791ca413a82961de1beee
Ekman <my.ekman@sweco.se>
2024-02-19 16:15:03 +0100

    stavfel

--- frontend/src/pages/SearchFishes.js
+++ frontend/src/pages/SearchFishes.js
@@ -12,7 +12,7 @@ function SearchBigFishes({ onSearch }) {
       <Row>
       <Col xs={6}>
       <div className={fishstyles.filtermessage}>
-        <h5>Vilka är de poppuläraste idéerna just nu?</h5>
+        <h5>Vilka är de populäraste idéerna just nu?</h5>
         <Button className={fishstyles.sökbtn2} variant="success" onClick={handleShowLargestClick}>Visa storbaddarna!</Button>
       </div>
       </Col>
